55db3c32c3b2c7ca32130a75face60ec959c86b5
ciec6574 <2283199586@qq.com>
2018-09-29 09:42:11 +0800

    Initial Commit

---
+++ test_6574/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  test_6574
+//
+//  Created by s20171106574 on 2018/9/29.
+//  Copyright © 2018年 s20171106574. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
